10355d21aee5d86fbb78c7e5c04833bbf90afd81
Carlosmercg <carlosdavidmercadogallego@gmail.com>
2024-10-03 11:18:55 -0500

    Update: Se hizo inicio de sesion y registro auto, views

--- src/main/java/com/asesinosdesoftware/javeparking/HelloController.java
+++ src/main/java/com/asesinosdesoftware/javeparking/HelloController.java
@@ -2,6 +2,7 @@ package com.asesinosdesoftware.javeparking;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
@@ -30,6 +31,35 @@ public class HelloController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    // Método para cargar y mostrar la vista de Inicio de sesion
+    @FXML
+    private void InicioSesion() {
+        try {
+            // Carga la vista desde el archivo FXML
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("Inicio_sesion_view.fxml"));
+            // Establece la vista cargada en el centro del contenedor principal
+            contenedor.setCenter(pane);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showError(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    // Método para mostrar un mensaje de éxito
+    private void showSuccess(String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Éxito");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 
 }
--- src/main/java/com/asesinosdesoftware/javeparking/controller/Registro_View_Controller.java
+++ src/main/java/com/asesinosdesoftware/javeparking/controller/Registro_View_Controller.java
@@ -3,6 +3,7 @@ package com.asesinosdesoftware.javeparking.controller;
 import com.asesinosdesoftware.javeparking.entities.Cliente;
 import com.asesinosdesoftware.javeparking.repository.ClienteRepository;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
@@ -34,11 +35,30 @@ public class Registro_View_Controller {
             ClienteRepository repository = new ClienteRepository();
 
             repository.agregarCliente(connection,C);
+            showSuccess("Registro de cliente exitoso");
 
         } catch (Exception e) {
             e.printStackTrace();
+            showError("Registro de cliente Fallido");
 
         }
+    }
+
+    private void showError(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    // Método para mostrar un mensaje de éxito
+    private void showSuccess(String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Éxito");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 
 }
