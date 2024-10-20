fc39821ddbbf4315f5513faa80053258063ce3a9
Carlosmercg <carlosdavidmercadogallego@gmail.com>
2024-10-20 02:57:16 -0500

    Update:Correcciones en PagoRepository

--- src/main/java/com/asesinosdesoftware/javeparking/repository/PagoRepository.java
+++ src/main/java/com/asesinosdesoftware/javeparking/repository/PagoRepository.java
@@ -9,14 +9,16 @@ public class PagoRepository {
 
     // Método para registrar un pago de reserva
     public static void agregarPagoReserva(Connection connection, PagoReserva pagoReserva) throws SQLException {
-        String sql = "INSERT INTO Pago (reserva_id, valor, fecha_pago) VALUES (?, ?, ?)";
+        String sql = "INSERT INTO pagoReserva (reservaID, valor, fechaPago, metodoPago) VALUES (?, ?, ?, ?)";
         try (PreparedStatement ps = connection.prepareStatement(sql)) {
-            ps.setInt(1, pagoReserva.getReserva().getId());
-            ps.setDouble(2, pagoReserva.getValor());
-            ps.setObject(3, pagoReserva.getFechaPago());
+            ps.setInt(1, pagoReserva.getReserva().getId()); // ID de la reserva
+            ps.setDouble(2, pagoReserva.getValor()); // Valor del pago
+            ps.setObject(3, pagoReserva.getFechaPago()); // Fecha del pago
+            ps.setString(4, pagoReserva.getMetodoPago()); // Método de pago
             ps.executeUpdate();
         }
     }
+
 
 }
 
