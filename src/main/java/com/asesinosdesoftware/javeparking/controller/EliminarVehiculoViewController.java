79600f063cc27222fc503f6764fe930b5f1b4f16
Carlosmercg <carlosdavidmercadogallego@gmail.com>
2024-10-19 19:53:02 -0500

    Update: Se creo la pantalla y el controlador para el caso de eliminacion de vehiculos, se añadio una funcion de eliminar vehiculos en el repositorio y el caso de uso es funcional

---
+++ src/main/java/com/asesinosdesoftware/javeparking/controller/EliminarVehiculoViewController.java
@@ -0,0 +1,68 @@
+package com.asesinosdesoftware.javeparking.controller;
+
+import com.asesinosdesoftware.javeparking.entities.Cliente;
+import com.asesinosdesoftware.javeparking.entities.Sesion;
+import com.asesinosdesoftware.javeparking.entities.Vehiculo;
+import com.asesinosdesoftware.javeparking.repository.ClienteRepository;
+import com.asesinosdesoftware.javeparking.repository.VehiculoRepository;
+import com.asesinosdesoftware.javeparking.services.JDBCService;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+
+import java.sql.Connection;
+
+public class EliminarVehiculoViewController {
+
+    @FXML
+    public TextField IDPlaca;
+
+    @FXML
+    private void   EliminarVehiculo(){
+    try {
+
+        Cliente dueno = new Cliente();
+        ClienteRepository CR = new ClienteRepository();
+
+        Vehiculo V = new Vehiculo();
+        VehiculoRepository VR = new VehiculoRepository();
+
+        JDBCService controller = new JDBCService();
+        Connection connection = controller.getConnection();
+
+        CR.buscarCliente(connection,Sesion.getcedula(),dueno);
+        VR.buscarVehiculo(connection,IDPlaca.getText(),V);
+
+        if(dueno.getId()==V.getClienteid()){
+            VR.eliminarVehiculo(connection,V);
+            showSuccess("Vehiculo eliminado correctamente");
+        }
+        else showError("El vehiculo no te pertenece");
+
+
+    }catch (Exception e) {
+        showError("Error eliminar el Vehiculo");
+        e.printStackTrace();
+
+        }
+
+    }
+
+    private void showError(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    // Método para mostrar un mensaje de éxito
+    private void showSuccess(String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Éxito");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+}
