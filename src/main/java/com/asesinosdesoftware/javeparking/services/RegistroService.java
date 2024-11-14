c5b33904b8d8172c377921fe9ba705b7f8de6cbf
Carlosmercg <carlosdavidmercadogallego@gmail.com>
2024-11-13 19:05:41 -0500

    Update: Se migro registro a servicio

--- src/main/java/com/asesinosdesoftware/javeparking/services/RegistroService.java
+++ src/main/java/com/asesinosdesoftware/javeparking/services/RegistroService.java
@@ -1,9 +1,28 @@
 package com.asesinosdesoftware.javeparking.services;
 
+import com.asesinosdesoftware.javeparking.entities.Cliente;
+import com.asesinosdesoftware.javeparking.exceptions.RepositoryException;
+import com.asesinosdesoftware.javeparking.exceptions.ServiceException;
 import com.asesinosdesoftware.javeparking.repository.ClienteRepository;
+
+import javax.sql.rowset.serial.SerialException;
+import java.sql.SQLException;
 
 public class RegistroService {
     ClienteRepository clienteRepository = new ClienteRepository();
 
+    public void registro(String nombre,String apellido,String cedula,String contrasena,String IdUni, Cliente cliente) throws SQLException, RepositoryException, ServiceException {
 
+        if (clienteRepository.buscarCliente(cedula,cliente)!=null){
+            throw new ServiceException("El cliente ya existe");
+        }
+
+        cliente.setNombre(nombre);
+        cliente.setApellido(apellido);
+        cliente.setCedula(cedula);
+        cliente.setHash(PasswordService.hashPassword(contrasena));
+        cliente.setUniversidad(IdUni.charAt(0));
+
+        clienteRepository.agregarCliente(cliente);
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.asesinosdesoftware.javeparking {
     requires java.sql;
     requires spring.security.crypto;
     requires com.h2database;
+    requires java.sql.rowset;
 
     opens com.asesinosdesoftware.javeparking to javafx.fxml;
     opens com.asesinosdesoftware.javeparking.controller to javafx.fxml;
